d08139209b76cd3d18e2f7ff3b2dbb066234546c
Italo Souza Melo <italosm29@gmail.com>
2022-02-15 00:32:37 -0300

    Fix bugs

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,5 +2,7 @@ import React from "react";
 import "./Display.css";
 
 export default function display(props) {
-  return <div className="display">{props.value}</div>;
+  return <div className="display">
+            {props.value}
+          </div>;
 }
